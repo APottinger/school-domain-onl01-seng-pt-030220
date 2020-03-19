3cd04c1de3e1982343c772a6bdac5abe5788e4c2
Allen Phillip Pottinger <allenpottinger@gmail.com>
2020-03-19 04:03:58 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
-# code here!
+class School
+  def initialize(name)     #school can be initialized with name
+    @name = school_name 
+  end
+  
+  
+  
+  def add_student(student_name, grade)
+    @student_name = student_name
+    @grade = grade
+  end
+  
+  def add_student(student_name, grade)
+    @student_name
+    @grade
+  end
+  
+end
+
+school.add_student("Zac", 9)
+
